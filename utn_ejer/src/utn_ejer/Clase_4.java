6e21b891d3f45a0c16c927149c3a0cadfc12e913
Brenda <brentorres26@gmail.com>
2023-03-20 14:23:03 -0300

    Lectura de Archivos - java.io.Files

---
+++ utn_ejer/src/utn_ejer/Clase_4.java
@@ -0,0 +1,21 @@
+package utn_ejer;
+
+import java.io.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+//lectura de archivos
+public class Clase_4 {
+
+	public static void main(String[] args)throws IOException{
+		
+		
+		String archivo = "D:\\Users\\Brenda\\Desktop\\PruebasGit-UTN\\PrubaGit.txt";
+		for(String linea : Files.readAllLines(Paths.get(archivo))) {
+			System.out.println(linea);
+		}
+		
+		
+	}
+}
