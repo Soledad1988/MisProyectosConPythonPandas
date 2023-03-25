0baa320c5f129a16ab7f350342dbbb53f8d5241e
Brenda <brentorres26@gmail.com>
2023-03-25 09:38:36 -0300

    Testing - prueba test unitario itemCarrito

---
+++ utn_ejer/src/clase_6/ItemCompraTest.java
@@ -0,0 +1,27 @@
+package clase_6;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemCompraTest {
+
+	ItemCompra objItemCompraTest;
+	
+	@BeforeEach
+	public void setUp() {objItemCompraTest = new ItemCompra();}
+	
+	
+	@org.junit.jupiter.api.Test
+	void testcalcularSubtotal() {
+		objItemCompraTest.setCantidad(2);
+		objItemCompraTest.setProducto(new Producto("arroz",200));
+		
+		
+		double subTotal=objItemCompraTest.calcularSubtotal();
+		double subTotalEsperado = 400.0;
+		
+		boolean resultado = (subTotal == subTotalEsperado);
+		assertTrue(resultado);
+	}
+}
--- utn_ejer/src/module-info.java
+++ utn_ejer/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module utn_ejer {
+	requires org.junit.jupiter.api;
 }
